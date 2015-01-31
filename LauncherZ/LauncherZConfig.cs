6f4e6ab7c659c944e91ffb763f75e29a680ed17b
morriswmz <morriswmz@gmail.com>
2015-01-31 00:47:16 -0600

    Forgot to initialize properties :(

--- LauncherZ/LauncherZConfig.cs
+++ LauncherZ/LauncherZConfig.cs
@@ -7,7 +7,15 @@ namespace LauncherZ
     [Serializable]
     public class LauncherZConfig
     {
+
+        public LauncherZConfig()
+        {
+            Priorities = new Dictionary<string, double>();
+        }
+
         [JsonProperty("priorities")]
         public Dictionary<string, double> Priorities { get; private set; }
+
+        
     }
 }
