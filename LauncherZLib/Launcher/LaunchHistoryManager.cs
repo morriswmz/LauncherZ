1af74a8f754fb28d1b82bca549fefbddaaddf84a
morriswmz <morriswmz@gmail.com>
2015-04-29 17:35:55 -0500

    Simply moved launch history to a standalone class.

--- LauncherZLib/Launcher/LaunchHistoryManager.cs
+++ LauncherZLib/Launcher/LaunchHistoryManager.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace LauncherZLib.Launcher
 {
     /// <summary>
-    /// Manages lauch history.
+    /// Manages launch history.
     /// </summary>
-    [JsonObject(MemberSerialization.OptIn)]
+    [Serializable]
     public sealed class LaunchHistoryManager
     {
-        [JsonProperty("Recent")]
-        private LinkedList<string> _recent = new LinkedList<string>();
-        [JsonProperty("Top")]
-        private List<Tuple<string, int>> _top = new List<Tuple<string, int>>();
+        private LinkedList<string> _history;
 
-        private int _maxHistoryCount = 10;
+        private int _maxHistoryCount;
+
+        public LaunchHistoryManager() : this(32)
+        {
+            
+        }
+
+        public LaunchHistoryManager(int maxHistoryCount)
+        {
+            _maxHistoryCount = maxHistoryCount;
+            _history = new LinkedList<string>();
+        }
+
+        private LaunchHistoryManager(SerializationInfo info, StreamingContext context)
+        {
+            var saveHistory = (string[]) info.GetValue("History", typeof (string[]));
+            _history = new LinkedList<string>(saveHistory);
+            MaxHistoryCount = info.GetInt32("MaxHistoryCount");
+        }
 
         public int MaxHistoryCount
         {
@@ -27,20 +44,40 @@ namespace LauncherZLib.Launcher
             }
         }
 
-        public IEnumerator<string> GetRecentEnumerator()
+        public IEnumerable<string> History
         {
-            return _recent.GetEnumerator();
+            get { return _history; }
         }
 
         public void PushHistory(string input)
         {
-            
+            if (string.IsNullOrWhiteSpace(input))
+                return;
+            if (_history.Count > 0 && _history.First.Value != input)
+            {
+                _history.AddFirst(input);
+                TruncateHistory();
+            }
+            else
+            {
+                _history.AddFirst(input);
+            }
         }
 
         private void TruncateHistory()
         {
-            
+            if (_history.Count <= _maxHistoryCount)
+                return;
+            while (_history.Count > _maxHistoryCount)
+            {
+                _history.RemoveLast();
+            }
         }
-        
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("MaxHistoryCount", _maxHistoryCount);
+            info.AddValue("History", _history.ToArray(), typeof (string[]));
+        }
     }
 }
